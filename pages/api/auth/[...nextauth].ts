599f003bcb21401406c5b3572ce34b19d6b510d4
Nguyễn Quang Gia Thuận <GiaThuanNguyen213@gmail.com>
2023-02-25 19:24:21 +0700

    Add MongoDb Adapter to Save User Information after Login In

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,22 +1,22 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "src/utils/lib/mongodb";
 
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-      clientId: "22792954187-0kgia3rbn2j066baj3e4vk1cckton43d.apps.googleusercontent.com",
+      clientId:
+        "22792954187-0kgia3rbn2j066baj3e4vk1cckton43d.apps.googleusercontent.com",
       clientSecret: "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",
-      
     }),
-    
 
     // ...add more providers here
   ],
-  secret:"giathuan"
-  
-  
+  adapter: MongoDBAdapter(clientPromise),
+  secret: "giathuan",
 };
 
 export default NextAuth(authOptions);
